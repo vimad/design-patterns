cf383c73d335237f58a33808a64364b573704357
vinod.madubashana <vinod.madubashana@cloudsolution-sa.com>
2021-07-01 19:04:48 +0530

    visitor pattern impl

---
+++ src/main/java/behavioral/visitor/Visitor.java
@@ -0,0 +1,11 @@
+package behavioral.visitor;
+
+public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
+}
